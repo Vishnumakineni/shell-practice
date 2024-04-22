f1218e98c49db5df959d2388cb9371cd9cccff1b
Vishnumakineni <vishnuvardhan.makineni@gmail.com>
2024-04-22 17:48:35 +0530

    some-msg

--- 06-array.sh
+++ 06-array.sh
@@ -2,6 +2,6 @@
 
 movies=("lk" "pk" "mk")
 
-echo "first movie is {$movies[0]}"
-echo "second movie is {$movies[1]}"
-echo "third movie is {$movies[2]}"
+echo "first movie is ${movies[0]}"
+echo "second movie is ${$movies[1]}"
+echo "third movie is ${$movies[@]}"
